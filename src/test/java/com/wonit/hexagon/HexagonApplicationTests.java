6cc866e873887af72037d7fc4c5bcd2ddb0197a8
dhslrl321 <dhslrl321@gmail.com>
2022-07-28 18:36:57 +0900

    commit

---
+++ src/test/java/com/wonit/hexagon/HexagonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonit.hexagon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
