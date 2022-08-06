726671d365212961ca40da21f46836c661813ce0
dhslrl321 <dhslrl321@gmail.com>
2022-08-06 13:48:12 +0900

    commit

--- infrastructure/src/main/java/com/wonit/hexagon/order/repository/JpaOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wonit.hexagon.order.repository;
-
-import com.wonit.hexagon.order.entity.JpaOrderEntity;
-import com.wonit.hexagon.repository.OrderRepository;
-import org.springframework.data.repository.Repository;
-
-public interface JpaOrderRepository extends Repository<JpaOrderEntity, Long>, OrderRepository {
-}
